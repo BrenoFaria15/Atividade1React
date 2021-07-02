7e846af485a1d1f0fb7427a353573b11803ee515
Breno Faria <brenooliveirafaria@gmail.com>
2021-07-01 22:21:01 -0300

    Atividade 1 Commit

---
+++ atividade1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Componentes from './Componentes/componentes';
+
+function App() {
+  return (
+    <Componentes/>
+  );
+}
+
+export default App;
---
+++ atividade1/src/Componentes/componentes.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Componentes extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Atividade 1 - Primeiro Componente</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default Componentes;
